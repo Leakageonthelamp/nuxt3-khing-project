e36066a11f121ea4733cea9ac07e04366d8be2af
Kittitat Chainonthi <kittitatchainonthi@admins-MacBook-Pro-4.local>
2023-03-13 00:26:05 +0700

    fix lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   experimental: {
-    writeEarlyHints: true
+    writeEarlyHints: true,
   },
   app: {
     head: {
       title: 'Sidebase Khing',
-    }
+    },
   },
   modules: [
     '@sidebase/nuxt-auth',
@@ -14,15 +14,15 @@ export default defineNuxtConfig({
     '@nuxt/devtools',
     '@pinia/nuxt',
     'nuxt-lodash',
-    '@nuxtjs/google-fonts'
+    '@nuxtjs/google-fonts',
   ],
   css: ['@/assets/main.scss'],
   typescript: {
-    shim: false
+    shim: false,
   },
   googleFonts: {
     families: {
-      'Inter': [400, 500, 600, 700],
-    }
-  }
+      Inter: [400, 500, 600, 700],
+    },
+  },
 })
